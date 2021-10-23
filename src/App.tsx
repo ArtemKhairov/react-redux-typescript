f5c28aff04167a689a89f9175b23c02dab509882
ArtemKhairov <artemxairoff@yandex.ru>
2021-10-23 19:17:02 +0700

    add documentation for frontend

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import TodoList from "./components/TodoList";
 import { UserList } from "./components/UserList";
 
+/**
+ * 
+ * @returns React Application
+ */
 function App() {
   return (
     <>
---
+++ src/index.js
@@ -0,0 +1,8 @@
+/**
+ * Конструктор для приветствия 
+ * @param {string} value - Фраза
+ */
+function frase(value) {
+  return "Hello friend" + `${value}`;
+}
+export default frase;
